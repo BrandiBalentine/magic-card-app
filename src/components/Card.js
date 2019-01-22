1708909294c16b253c85e345653d64893076e7ae
Brandi Balentine <brandibalentine@gmail.com>
2019-01-21 22:14:32 -0600

    Fix missing images

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import image from '../images/no-image.jpg';
 
 const Card = ({ name, artist, imageUrl, setName, type, colors }) => {
   
   return (
     <li>
       <div className={`card card--${colors[0] && colors[0].toLowerCase()}`}>
-        <img src={imageUrl} alt={`${name} Magic Card`} />
+    
+        {imageUrl?
+          React.createElement("img", {src: imageUrl, alt:`${name} Magic Card`}) :
+          <img src={image}/>
+        }
+        
         <div className="card-details">
           <h2>{name}</h2>
           <p>Artist: {artist}</p>
