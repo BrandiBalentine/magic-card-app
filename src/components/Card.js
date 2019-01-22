fcd9ff10d3ea98f99dd6d31dfb4378341ff4b81e
Brandi Balentine <brandibalentine@gmail.com>
2019-01-21 19:25:17 -0600

    fix missing color bug

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({ name, artist, imageUrl, setName, type, colors }) => {
   
   return (
     <li>
-      <div className={`card card--${colors && colors[0].toLowerCase()}`}>
+      <div className={`card card--${colors[0] && colors[0].toLowerCase()}`}>
         <img src={imageUrl} alt={`${name} Magic Card`} />
         <div className="card-details">
           <h2>{name}</h2>
